9221bef49925d03b65a5662923e468e1633b93a0
yashghori <yashghori2804@gmail.com>
2022-05-25 00:03:47 +0530

    version with basic functionality

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -4,7 +4,7 @@ const Song = ({ currentSong, isPlaying, isDark }) => {
   return (
     <div className="song_cointainer">
       <img className={`cover_image ${isDark ? "shadow_dark" : ""} ${isPlaying ? "spin_image" : ""}`} src={currentSong.cover} alt="" />
-      <h2 className={` song_name ${isDark ? "dark_sodark_song_nameng_name" : ""} `} >{currentSong.name}</h2>
+      <h2 className={` song_name ${isDark ? "dark_song_name" : ""} `} >{currentSong.name}</h2>
       <h3 className="song_artist">{currentSong.artist}</h3>
     </div>
   );
